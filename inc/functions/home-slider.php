6e25c304c033e988dc0bfcbb5ef7ebcb63b13462
skater2014 <sanohayakazu@gmail.com>
2026-03-07 21:18:02 +0900

    Swiper対応のスライダーを更新

--- inc/functions/home-slider.php
+++ inc/functions/home-slider.php
@@ -1,61 +1,44 @@
 <?php
-
-//get_header('home'); //Slick navi
-get_header('77'); //Regular Menu Bootstrap navi
+// get_header('home'); // 旧Slick用
+get_header('77'); // Regular Menu
 
 $slide_ids = array();
 
 $slider = array(
+  'post_type'           => 'post',
+  'meta_key'            => 'show_in_slider',
+  'meta_value'          => 'yes',
+  'posts_per_page'      => -1,
+  'ignore_sticky_posts' => true,
+);
 
-        'post_type' => 'post',
+$the_query = new WP_Query($slider);
 
-        'meta_key' => 'show_in_slider',
-
-        'meta_value' => 'yes',
-
-        'posts_per_page' => -1,
-
-        'ignore_sticky_posts' => true
-
-        );
-
-$the_query = new WP_Query( $slider );
-
-   if ( $the_query->have_posts() ) :
-
+if ($the_query->have_posts()) :
 ?>
+  <div class="home-slider swiper-home invisibility">
+    <div class="article">
+      <div class="swiper home-swiper" id="home-swiper">
+        <div class="swiper-wrapper">
+          <?php
+          while ($the_query->have_posts()) :
+            $the_query->the_post();
+            $slide_ids[] = get_the_ID();
+            get_template_part('slide', 'template');
+          endwhile;
+          ?>
+        </div>
 
-    <div class="home-slider invisibility">
-
-      <div class="article">
-
-        <div id="home-slider">
-
-      <?php
-
-      while ( $the_query->have_posts() ) : $the_query->the_post();
-
-        array_push($slide_ids, $post->ID);
-
-        get_template_part( 'slide', 'template' );
-
-      endwhile;
-
-      ?>
-
-        </div><!-- end home-slider -->
-
-      </div> <!--end article -->
-
-    </div><!--end home-slider invisibility -->
-
-<?php endif; ?>
-
-
-
-
-
+        <div class="swiper-button-prev" aria-label="前へ"></div>
+        <div class="swiper-button-next" aria-label="次へ"></div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
+  </div>
 <?php
-//get_footer('home1');
+endif;
 
+wp_reset_postdata();
+
+// get_footer('home1');
 ?>
--- slide-template.php
+++ slide-template.php
@@ -1,47 +1,57 @@
-<div class="home-slide">
-    <div class="home-slide-feature">
-        <?php
-        $type = get_post_meta($post->ID, 'page_featured_type', true);
+<?php
+$type = get_post_meta(get_the_ID(), 'page_featured_type', true);
+?>
 
-        switch ($type) {
-            // case 'youtube-iframe':
-            // echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.get_post_meta( get_the_ID(), 'page_video_id', true ).'?wmode=transparent" frameborder="0" allowfullscreen></iframe>';
-            
-            case 'youtube':
-                echo '<lite-youtube videoid="' . get_post_meta(get_the_ID(), 'page_video_id', true) . '" playlabel="Play: Keynote (Google I/O \'18)" style="max-width:100% height="517px; height:auto;"></lite-youtube>';
-                break;
+<div class="swiper-slide">
+    <div class="home-slide">
+        <div class="home-slide-feature">
+            <?php
+            switch ($type) {
+                case 'youtube':
+                    $video_id = get_post_meta(get_the_ID(), 'page_video_id', true);
+                    if (!empty($video_id)) {
+                        echo '<lite-youtube videoid="' . esc_attr($video_id) . '" playlabel="' . esc_attr(get_the_title()) . '"></lite-youtube>';
+                    }
+                    break;
 
-            case 'vimeo':
-                // echo '<iframe src="https://player.vimeo.com/video/'.get_post_meta( get_the_ID(), 'page_video_id', true ).'?title=0&amp;byline=0&amp;portrait=0&amp;color=03b3fc" width="500" height="338" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
-                echo '<lite-vimeo videoid="' . get_post_meta(get_the_ID(), 'page_video_id', true) . '">
-                    <div class="ltv-playbtn"></div>
-                </lite-vimeo>';
-                break;
+                case 'vimeo':
+                    $video_id = get_post_meta(get_the_ID(), 'page_video_id', true);
+                    if (!empty($video_id)) {
+                        echo '<lite-vimeo videoid="' . esc_attr($video_id) . '"><div class="ltv-playbtn"></div></lite-vimeo>';
+                    }
+                    break;
 
-            default:
-                echo '<div class="home-slide-image">';
-                echo '<a href="'.get_permalink().'">';
-                the_post_thumbnail('large');
-                echo '</a>';
-                echo '</div>';
-                break;
-        }
-        ?>
-    </div><!-- home-slide-feature -->
+                default:
+                    echo '<div class="home-slide-image">';
+                    echo '<a href="' . esc_url(get_permalink()) . '">';
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail('large');
+                    } else {
+                        echo '<img src="' . esc_url(get_template_directory_uri() . '/images/no-image.jpg') . '" alt="' . esc_attr(get_the_title()) . '">';
+                    }
+                    echo '</a>';
+                    echo '</div>';
+                    break;
+            }
+            ?>
+        </div><!-- /.home-slide-feature -->
 
-    <div class="home-slide-info">
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <div class="home-slide-info">
+            <h3>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
 
-        <div class="home-slide-text">
-            <?php echo dess_get_excerpt(60); ?>
-            <?php 
-            // 物件情報テーブルを表示
-            if (file_exists(get_template_directory() . '/property-info-table.php')) {
-                get_template_part('property-info-table'); // 物件情報テーブルの読み込み
+            <div class="home-slide-text">
+                <?php echo wp_kses_post(dess_get_excerpt(60)); ?>
+            </div>
+
+            <?php
+            if (locate_template('property-info-table.php')) {
+                get_template_part('property-info-table');
             } else {
                 echo '<p>物件情報がありません</p>';
             }
             ?>
-        </div><!-- home-slide-text -->
-    </div><!-- home-slide-info -->
-</div><!-- home-slide -->
+        </div><!-- /.home-slide-info -->
+    </div><!-- /.home-slide -->
+</div><!-- /.swiper-slide -->
