a87721c19da07947cbc62ef5712e73ff21707354
skater2014 <sanohayakazu@gmail.com>
2026-03-20 00:06:35 +0900

    Clean up search form selectors and placeholder styles

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /****************************************
 
 	page.php　test
@@ -9,29 +10,35 @@
 	page.php のコードに関しては、
 	CHAPTER 12 で詳しく解説しています。
 
-*****************************************/
+ *****************************************/
 
 get_header('77'); ?>
 <!-- page.php -->
 <div id="main">
-	<?php if ( have_posts() ) : /** WordPress ループ */
-		while ( have_posts() ) : the_post(); /** 繰り返し処理開始 */ ?>
+	<?php if (have_posts()) :
+		/** WordPress ループ */
+		while (have_posts()) : the_post();
+			/** 繰り返し処理開始 */ ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				<?php the_content(); ?>
 			</div>
-		<?php endwhile; /** 繰り返し処理終了 */
-	else: /** ここから記事が見つからなかった場合の処理 */ ?>
-			<div class="post page">
-				<h2>ページがありません</h2>
-				<p>お探しのページは見つかりませんでした。</p>
-			</div>
-	<?php endif; /** WordPress ループここまで */ ?>
+		<?php endwhile;
+		/** 繰り返し処理終了 */
+	else:
+		/** ここから記事が見つからなかった場合の処理 */
+		?>
+		<div class="post page">
+			<h2>ページがありません</h2>
+			<p>お探しのページは見つかりませんでした。</p>
+		</div>
+	<?php endif;
+	/** WordPress ループここまで */ ?>
 </div>
 <!-- /main -->
 <!-- /page.php -->
 <div id="single">
-<?php get_sidebar('land');?>
+	<?php get_sidebar('land'); ?>
 </div>
 
 
